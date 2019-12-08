e4ada2552d147a526d77d3679ef202ff3d1b0c30
Tomasz Reczek <tommy222@op.pl>
2019-12-08 17:39:53 +0100

    last update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import SearchCity from "./SearchCity";
+import ViewWeather from "./ViewWeather";
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    value: '',
+    date: '',
+    country: '',
+    city: '',
+    temp: '',
+    minTemp: '',
+    maxTemp: '',
+    pressure: '',
+    humidity: '',
+    wind: '',
+    coord_lat: '',
+    coord_lon: '',
+    error: false,
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      value: e.target.value,
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=05508bb378ad891b493b0c886cca7a57&units=metric&lang=pl`;
+    axios.get(API)
+      .then(resp => resp.data)
+      .then(data => {
+        const time = new Date().toLocaleString();
+        const { value } = this.state;
+        this.setState({
+          error: false,
+          date: time,
+          city: value,
+          country: data.sys.country,
+          temp: data.main.temp,
+          minTemp: data.main.temp_min,
+          maxTemp: data.main.temp_max,
+          pressure: data.main.pressure,
+          humidity: data.main.humidity,
+          wind: data.wind.speed,
+          coord_lat: data.coord.lat,
+          coord_lon: data.coord.lon,
+        })
+      })
+      .catch(error => {
+        console.log(error);
+        const { value } = this.state;
+        this.setState({
+          error: true,
+          city: value,
+        })
+      })
+
+  }
+
+  render() {
+    const {value}=this.state;
+    return (
+      <>
+      <div className="main-container p-4 ">
+      <div className="row">
+      <h1 className='col-12 my-5 text-center'>Aplikacja pogodowa 2019</h1>
+      </div>
+        <SearchCity value={value} change={this.handleChange} submit={this.handleSubmit} />
+        <ViewWeather weather={this.state} />
+      </div>
+      </>
+    );
+  }
 }
-
 export default App;
+
+
---
+++ src/ViewWeather.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import './App.css';
+
+const ViewWeather = (props) => {
+    const {
+        city,
+        country,
+        date,
+        temp,
+        minTemp,
+        maxTemp,
+        pressure,
+        humidity,
+        wind,
+        weather,
+        coord_lat,
+        coord_lon,
+        error
+    } = props.weather
+
+const info = (
+    <div className="row">
+    <h2 className='col-12 p-3 text-warning bg-secondary text-center'>Nie posiadamy w bazie danych miasta <span className= 'text-danger px-2'>{city.toUpperCase()}</span></h2>
+    </div>
+)
+
+    let output = null;
+
+    if (!error && city) {
+        output = (
+            
+            <div className="container text-center">
+            <div className="cards py-5 p-5 card-color">
+            <div className="row">
+                <h1 className='col-6 text-center text-success'>{city.toUpperCase()}</h1>
+                <h1 className='col-6'>{country.toUpperCase()}</h1>
+                </div>
+                <div className="row"> 
+                <h2 className='py-4 col-6'>{date}</h2>
+                <h2 className='py-4 col-6'>Temperatura: {temp}&deg;C</h2>
+                </div>
+                <div>{weather}</div>
+                <div className="row mb-2">
+                <div className="col-6">
+                <h4>Minimalna temperatura: <span className="text-danger">{minTemp}&deg;C</span></h4>
+                </div>
+                <div className="col-6">
+                <h4>Maksymalna temperatura: <span className="text-success">{maxTemp}&deg;C</span></h4>
+                </div>
+                </div>
+                <div className="row mb-2">
+                <div className="col-6">
+                <h4>Ciśnienie atmosferyczne: {pressure}HPa</h4>
+                </div>
+                <div className="col-6">
+                <h4>Wilgotność powietrza: {humidity}%</h4>
+                </div>
+                </div>
+                <div className="row">
+                <div className="col-6">
+                <h4>Siła wiatru: {wind}m/s</h4>
+                </div>
+                <div className="col-6">
+                <h4>Koordynaty: {coord_lat}N, {coord_lon}E</h4>
+                </div>
+                </div>
+                </div>
+                </div>
+        )
+    }
+    return (
+        <div>{error ? info: output}</div>
+    );
+}
+
+export default ViewWeather;
