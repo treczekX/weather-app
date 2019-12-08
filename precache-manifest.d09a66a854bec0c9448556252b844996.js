6837737bef7b99b16af4353c070cf9f1196926dd
Tomasz Reczek <tommy222@op.pl>
2019-12-08 17:48:55 +0100

    Updates

---
+++ precache-manifest.d09a66a854bec0c9448556252b844996.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d344103db23e459394f0062f9521091",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "03be1ec2c80121d7cb90",
+    "url": "/weather-app/static/css/main.0fe4144b.chunk.css"
+  },
+  {
+    "revision": "b3a69eb44a2d4a2970ac",
+    "url": "/weather-app/static/js/2.c5d0602c.chunk.js"
+  },
+  {
+    "revision": "03be1ec2c80121d7cb90",
+    "url": "/weather-app/static/js/main.f4b89eb6.chunk.js"
+  },
+  {
+    "revision": "20b7fe9aece0c845f972",
+    "url": "/weather-app/static/js/runtime-main.630a9fd3.js"
+  },
+  {
+    "revision": "c8ecc586f2d5b9c900647b790e81d003",
+    "url": "/weather-app/static/media/pic.c8ecc586.jpg"
+  }
+]);
